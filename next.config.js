1247d8202850161c551e612067253c29bc32feb4
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-24 14:20:43 +0900

    svgr 플러그인 적용

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other next config items
+
+  webpack: config => {
+    // ... other rules
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack'
+        },
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'static/[path][name].[ext]'
+          }
+        }
+      ],
+      type: 'javascript/auto',
+      issuer: {
+        and: [/\.(ts|tsx|js|jsx|md|mdx)$/]
+      }
+    });
+
+    return config;
+  }
+};
+
+module.exports = nextConfig;
--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-//import { ReactComponent as LogoSvg } from '../../../public/img/logo.svg';
+import { ReactComponent as LogoSvg } from '../../../public/img/logo.svg';
 
 export default function Logo () {
+
   return (
     <i>
-      {/* <LogoSvg /> */}
+      <LogoSvg />
     </i>
   );
 }
