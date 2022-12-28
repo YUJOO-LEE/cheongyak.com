5b20775264903153887026f4959eb0490c07d160
YUJOO-LEE <lllllllllee@gmail.com>
2022-12-28 22:20:13 +0900

    og meta _app 으로 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>청약닷컴</title>
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='청약닷컴' />
+        <meta property='og:url' content='cheongyak.com' />
+        <meta property='og:description' content='신도시 청약 정보' />
+        <meta property='og:image' content='img/metaimg.png' />
       </Head>
       <Component {...props} />
     </Provider>
