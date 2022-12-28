e0568c67960cb02e3656322f10b57c7f1e83e975
YUJOO-LEE <lllllllllee@gmail.com>
2022-12-28 22:04:49 +0900

    og meta 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,11 @@ export default class MyDocument extends Document {
             name='description'
             content='검단신도시, 운정신도시 청약 정보'
           />
+          <meta property='og:type' content='website' />
+          <meta property='og:title' content='청약닷컴' />
+          <meta property='og:url' content='cheongyak.com' />
+          <meta property='og:description' content='신도시 청약 정보' />
+          <meta property='og:image' content='img/metaimg.png' />
           <script src='https://kit.fontawesome.com/08c501c945.js' crossOrigin='anonymous'></script>
           <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=0f018f7ff64a71f0976e2448a4ec8cea&autoload=false"></script>
 
